cfa6df956e5bf35cd8e0e9e0f0f2ad946980f518
thkim <thkim@interfaceit.co.kr>
2025-04-01 11:06:31 +0900

    [환경설정 추가] 1. application.yml 파일 분리 (공통, local) 2. 의존성 추가 (JWT, Security, swagger, validation) 3. Security, JWT 클래스 정의

---
+++ src/main/java/com/lims/lims_study/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,45 @@
+package com.lims.lims_study.jwt;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    public JwtAuthenticationFilter(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+        String header = request.getHeader("Authorization");
+        String username = null;
+        String jwt = null;
+
+        if (header != null && header.startsWith("Bearer ")) {
+            jwt = header.substring(7);
+            username = jwtUtil.getUsernameFromToken(jwt);
+        }
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            if (jwtUtil.validateToken(jwt)) {
+                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(username, null, null);
+                auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        }
+        chain.doFilter(request, response);
+    }
+}
